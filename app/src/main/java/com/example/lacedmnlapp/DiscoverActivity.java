755905e6640eee19dfd14182e89fb73c5d1edb93
Stephen Jed Fetalino <96241556+Landotin@users.noreply.github.com>
2024-11-27 13:00:47 +0800

    3rd

--- app/src/main/java/com/example/lacedmnlapp/DiscoverActivity.java
+++ app/src/main/java/com/example/lacedmnlapp/DiscoverActivity.java
@@ -1,6 +1,33 @@
 package com.example.lacedmnlapp;
 
-public class DiscoverActivity {
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DiscoverActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerViewCategory;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.discoverpage);
+
+        recyclerViewCategory();
+    }
+
+    private void recyclerViewCategory() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewCategory = findViewById(R.id.recyclerView);
+        recyclerViewCategory.setLayoutManager(linearLayoutManager);
+    }
 
 
 }
