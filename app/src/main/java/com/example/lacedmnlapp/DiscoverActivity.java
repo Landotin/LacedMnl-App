d2a6773a9bb70acc988f4e106678640da906130b
Stephen Jed Fetalino <96241556+Landotin@users.noreply.github.com>
2024-11-27 13:41:19 +0800

    3rd

--- app/src/main/java/com/example/lacedmnlapp/DiscoverActivity.java
+++ app/src/main/java/com/example/lacedmnlapp/DiscoverActivity.java
@@ -1,15 +1,14 @@
 package com.example.lacedmnlapp;
 
 import android.os.Bundle;
-import android.widget.LinearLayout;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.lacedmnlapp.Domain.CategoryDomain;
+
+import java.util.ArrayList;
 
 public class DiscoverActivity extends AppCompatActivity {
     private RecyclerView.Adapter adapter;
@@ -27,6 +26,14 @@ public class DiscoverActivity extends AppCompatActivity {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         recyclerViewCategory = findViewById(R.id.recyclerView);
         recyclerViewCategory.setLayoutManager(linearLayoutManager);
+
+        ArrayList<CategoryDomain> category=new ArrayList<>();
+        category.add(new CategoryDomain("Shoes 1", "adidas_1" ));
+        category.add(new CategoryDomain("Shoes 2", "nike_1"));
+        category.add(new CategoryDomain("Shoes 3", "adidas_2"));
+        category.add(new CategoryDomain("Shoes 4", "nike_2"));
+        category.add(new CategoryDomain("Shoes 5", "adidas_3"));
+
     }
 
 
